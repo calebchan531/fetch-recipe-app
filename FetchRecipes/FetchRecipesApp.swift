7c87409c2dba1e793467b2f6bb18abef0014484a
calebchan531 <chchan@ksu.edu>
2025-06-08 21:00:01 -0500

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Caleb Chan on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
